ee13d8c41900106209791565814b7d58243212bf
jhlake <jh.lake@uniandes.edu.co>
2019-03-03 21:24:09 -0500

    TODO IN FUTURE

--- models/usersInGroups.js
+++
@@ -1,9 +0,0 @@
-/*'use strict';
-module.exports = (sequelize, DataTypes) => {
-  const usersInGroups = sequelize.define('usersinGroups', {
-    uogId: {type:DataTypes.INTEGER, primaryKey:true},
-    isAdmin: DataTypes.BOOLEAN
-  }, {timestamps: false});
-
-  return usersInGroups;
-};*/
