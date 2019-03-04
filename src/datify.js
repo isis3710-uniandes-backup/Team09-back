27f563bbe7153e20621d7244d86ef20355d819e6
jhlake <jh.lake@uniandes.edu.co>
2019-03-03 19:02:51 -0500

    Small Changes

--- src/datify.js
+++ src/datify.js
@@ -1,14 +1,16 @@
 function pad2(n) {  // always returns a string
     return (n < 10 ? '0' : '') + n;
 }
-
-function getDate() {
-    d = new Date();
-    var yyyy = this.getFullYear().toString();
-    var MM = pad2(this.getMonth() + 1);
-    var dd = pad2(this.getDate());
-    var hh = pad2(this.getHours());
-    var mm = pad2(this.getMinutes());
-    var ss = pad2(this.getSeconds());
-    return `${yyyy}-${MM}-${dd} ${hh}:${mm}:${ss}`;
+module.exports={
+    getDate: function getDate() {
+        d = new Date();
+        var yyyy = d.getFullYear().toString();
+        var MM = pad2(d.getMonth() + 1);
+        var dd = pad2(d.getDate());
+        var hh = pad2(d.getHours());
+        var mm = pad2(d.getMinutes());
+        var ss = pad2(d.getSeconds());
+        return `${yyyy}-${MM}-${dd}` + " " + `${hh}:${mm}:${ss}`;
+    }
 }
+
--- src/routes/messages.js
+++ src/routes/messages.js
@@ -1,7 +1,7 @@
 //const express = require('express');
 //const app = express();
 const db = require(__dirname.slice(0,__dirname.length-11)+'/models/');
-const datify = require('../datify');
+const datify = require('../datify.js');
 
 module.exports={
 	getMessages: function(req, res){
